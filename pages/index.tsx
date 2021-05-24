d624c64757fd954d93214bde490e7de490f65c9c
Zinochan <devzinochan@gmail.com>
2021-05-24 12:47:51 +0100

    fix env variables error

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Navbar from "@components/Navbar";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
   return (
     <section
       style={{ backgroundImage: "url(/images/home-bg.jpg)" }}
